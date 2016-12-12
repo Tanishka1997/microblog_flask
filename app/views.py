32635ce1804bccbcb51ab094cdf1dc0387974725
Tanishka1997 <u15co059@coed.svnit.ac.in>
2016-12-13 00:22:32 +0530

    forms

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from app import app
-from flask import render_template
+from flask import render_template,flash,redirect
+from .forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'nickname': 'Tanishka'}	
+	user = {'nickname': 'Tanishka'}
 	return render_template('index.html',title='Home',user=user)
+
+@app.route('/login',methods=['GET','POSTS'])
+def login():
+	form=LoginForm()
+	return render_template('login.html',title='Sign In',form=form)
