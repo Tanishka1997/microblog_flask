033a797f6e1e1635cc83d088c9afc92cc67e892f
Tanishka1997 <u15co059@coed.svnit.ac.in>
2016-12-16 23:54:44 +0530

    forms

--- app/views.py
+++ app/views.py
@@ -7,7 +7,11 @@ def index():
 	user = {'nickname': 'Tanishka'}
 	return render_template('index.html',title='Home',user=user)
 
-@app.route('/login',methods=['GET','POSTS'])
+@app.route('/login',methods=['GET','POST'])
 def login():
 	form=LoginForm()
+	if form.validate_on_submit():
+		flash('Login form %s'%(form.openid.data))
+		return redirect('/index')
+
 	return render_template('login.html',title='Sign In',form=form)
