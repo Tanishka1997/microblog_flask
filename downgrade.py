e7b1a2fe3c3d0ce691c86debfe94bbf66e145aa8
Tanishka1997 <u15co059@coed.svnit.ac.in>
2016-12-22 20:07:56 +0530

    databse_learning

---
+++ downgrade.py
@@ -0,0 +1,7 @@
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+from app import db
+from migrate.versioning import api
+api.downgade(SQLALCHEMY_DATABASE_URI,SQLALCHEMY_MIGRATE_REPO)
+v=api.db_version(SQLALCHEMY_DATABASE_URI,SQLALCHEMY_MIGRATE_REPO)
+print("VERSION: "+str(v))
