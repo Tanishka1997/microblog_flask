6b227f9ce2eb8f5ab00f2bf36a01adf34953e690
Tanishka1997 <u15co059@coed.svnit.ac.in>
2016-12-23 19:27:06 +0530

    login

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_openid import OpenId
+from config import basdir
 app=Flask(__name__)
 app.config.from_object('config')
 db=SQLAlchemy(app)
+lm=LoginManager()
+lm.init_app(app)
+oid=OpenId(app,os.path.join(basedir,'tmp'))
 from app import views,models
